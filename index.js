d098dac335a6c751f77b3e54a9e32c0bfa13125d
DEVARSHIP1707 <202401445@daiict.ac.in>
2026-07-05 01:05:59 +0530

    Completed

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app= express()
 const port=5173
 app.use(express.json())
+ const usersRouter=require("./Routes/users");
+ const booksRouter=require("./Routes/books");
 app.get('/',(req,res)=>{
     res.status(200).send('Welcome to Library Management System')
 })
-//For any url not mentioned above 
+  app.use("/users",usersRouter)
+app.use("/books",booksRouter)
+//for any url not mentioned
 app.use((req,res)=>{
-    res.status(404).send('Page Not Fount')
+    res.status(404).send('Page Not Found')
 })
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
